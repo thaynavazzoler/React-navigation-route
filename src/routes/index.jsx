1c56ef48877e7b046a0164b95a5613ae1a7bd14d
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-03-13 08:20:30 -0300

    "feat create drawer navigation"

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,11 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 
 import StackRoutes from "./stack.routes";
+import TabRoutes from "./tab.routes";
+import DrawerRoutes from "./drawer.routes";
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {/*<tackRoutes />*/}
+      {/*<TabRoutes />*/}
+      <DrawerRoutes />
       <StatusBar style="auto" />
     </NavigationContainer>
   );
